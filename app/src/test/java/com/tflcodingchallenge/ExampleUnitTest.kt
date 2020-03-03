26dc9d9b6242ac4190ba5a7d330babeb20651675
nebilseid <nebilseid@gmail.com>
2020-03-03 12:02:11 +0000

    unit test started

--- app/src/test/java/com/tflcodingchallenge/ExampleUnitTest.kt
+++ app/src/test/java/com/tflcodingchallenge/ExampleUnitTest.kt
@@ -1,17 +1,42 @@
 package com.tflcodingchallenge
 
+import com.tflcodingchallenge.base.Presenter
+import com.tflcodingchallenge.home.RoadStatusPresenter
+import com.tflcodingchallenge.model.RoadStatus
+import com.tflcodingchallenge.network.RoadStatusService
+import io.reactivex.Observable
+import io.reactivex.Single
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.junit.runners.BlockJUnit4ClassRunner
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import retrofit2.Response
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+@RunWith(BlockJUnit4ClassRunner::class)
+class RoadStatusPresenterTest {
+
+    @Mock
+    private lateinit var roadStatusService: RoadStatusService
+
+    @Mock
+    private lateinit var roadStatusPresenter: RoadStatusPresenter
+
+    @Before
+    fun setup(){
+        MockitoAnnotations.initMocks(this)
+        roadStatusPresenter = RoadStatusPresenter(roadStatusService)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `is successful - Response as result`() {
+
+
+
+        }
     }
 }
